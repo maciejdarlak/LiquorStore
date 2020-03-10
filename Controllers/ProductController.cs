0b37fac3d683ae0ae52d7f24bcb98e030bea938a
maciejdarlak <maciej.darlak@gmail.com>
2020-03-10 17:24:38 +0100

    Added Product Controller and View

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LiquorStore.Data;
+
+namespace LiquorStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ProductContext _productContext;
+
+        public ProductController (ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
