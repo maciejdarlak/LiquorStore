918e94256faa3e6fbe236a2aefd98045c3009509
maciejdarlak <maciej.darlak@gmail.com>
2020-03-10 17:23:43 +0100

    Added View Components

---
+++ Components/TopNavbarComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LiquorStore.Data;
+using LiquorStore.Models;
+
+namespace LiquorStore.ViewComponents
+{
+    public class TopNavbarComponent : ViewComponent
+    {
+        private ProductContext _productContext;
+        public TopNavbarComponent(ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _productContext.Product.Select(x => x.Category).Distinct().OrderBy(x => x);
+            return View(categories);
+        }
+    }
+}
