b11fa1c6b0976245f845429464b007a8044d6dc0
maciejdarlak <maciej.darlak@gmail.com>
2020-03-11 16:04:26 +0100

    Added Create methods

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LiquorStore.Data;
+using LiquorStore.Models;
 
 namespace LiquorStore.Controllers
 {
@@ -20,5 +21,26 @@ namespace LiquorStore.Controllers
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id, Name, Category, SubCategory, ProductionYear, Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productContext.Add(product);
+                _productContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+
+
     }
 }
