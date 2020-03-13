c402f055b48ca5e375944b4b98eef59a99f1d89e
maciejdarlak <maciej.darlak@gmail.com>
2020-03-13 19:16:15 +0100

    no message

--- Startup.cs
+++ Startup.cs
@@ -54,7 +54,7 @@ namespace LiquorStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Home}/{id?}");
             });
         }
     }
