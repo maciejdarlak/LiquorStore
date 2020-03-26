079c7623120fd4ac3f45243d18877a51c9765021
maciejdarlak <maciej.darlak@gmail.com>
2020-03-26 19:01:45 +0100

    Added categories and subcategories to TopNavbar

---
+++ Components/BeersSubCategoryNavigation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LiquorStore.Data;
+using LiquorStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiquorStore.Components
+{
+    public class BeersSubCategoryNavigation : ViewComponent
+    {
+        private ProductContext _productContext;
+        public BeersSubCategoryNavigation(ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var subcategories = await _productContext.Product.Where(x => x.Category == "Beers").Select(x => x.SubCategory).Distinct().ToListAsync();
+            return View(subcategories);
+        }
+    }
+}
---
+++ Components/SpiritsSubCategoryNavigation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LiquorStore.Data;
+using LiquorStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiquorStore.Components
+{
+    public class SpiritsSubCategoryNavigation : ViewComponent
+    {
+        private ProductContext _productContext;
+        public SpiritsSubCategoryNavigation(ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+                var subcategories = await _productContext.Product.Where(x => x.Category == "Spirits").Select(x => x.SubCategory).Distinct().ToListAsync();
+                return View(subcategories);           
+        }
+    }
+}
