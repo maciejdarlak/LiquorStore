338a7fa908e2dab9e8a0cbd062b45575328f1b08
maciejdarlak <maciej.darlak@gmail.com>
2020-05-10 20:57:35 +0200

    no message

---
+++ Abstract/ICart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiquorStore.Models;
+
+namespace LiquorStore.Abstract
+{
+    public interface ICart
+    {
+        public void AddItem(Product product, int quantity);
+    }
+}
